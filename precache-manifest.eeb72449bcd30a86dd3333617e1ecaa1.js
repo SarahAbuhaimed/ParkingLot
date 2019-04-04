8ee19f42daf71360f6a3e7d7c00758a21e3d4fe8
sarahAbuhaimed <sar-abu@hotmail.com>
2019-04-04 09:23:23 +0300

    Updates

---
+++ precache-manifest.eeb72449bcd30a86dd3333617e1ecaa1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcb5ef619bdd9c00c60cf82ee7f9311a",
+    "url": "/ParkingLot/static/media/dodger3.dcb5ef61.woff"
+  },
+  {
+    "revision": "ce8f7e87f2652af342665289f908e96e",
+    "url": "/ParkingLot/static/media/dodger3.ce8f7e87.eot"
+  },
+  {
+    "revision": "b655a9122f80ffd9c3162ca8e379f609",
+    "url": "/ParkingLot/static/media/dodger3.b655a912.ttf"
+  },
+  {
+    "revision": "723b4ba932b88899df6f71152e12d721",
+    "url": "/ParkingLot/static/media/dodger3.723b4ba9.svg"
+  },
+  {
+    "revision": "34fabd8aa13071653ead",
+    "url": "/ParkingLot/static/js/runtime~main.6fae4283.js"
+  },
+  {
+    "revision": "00245e3b5728647df8cb",
+    "url": "/ParkingLot/static/js/main.303c1fde.chunk.js"
+  },
+  {
+    "revision": "b0781cc8d320beb0866b",
+    "url": "/ParkingLot/static/js/2.c3ea6db5.chunk.js"
+  },
+  {
+    "revision": "00245e3b5728647df8cb",
+    "url": "/ParkingLot/static/css/main.638d8e1f.chunk.css"
+  },
+  {
+    "revision": "cc52570124b1ecefbed1d7b4e1f835ae",
+    "url": "/ParkingLot/index.html"
+  }
+];
